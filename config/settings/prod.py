08645a73dc1b0f340c293c12963a333419f8f021
Mahdi Alyan <mahdi.a@neruos.tech>
2026-02-26 19:57:58 +0200

    allowed hosts

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["yourusername.pythonanywhere.com"]
+ALLOWED_HOSTS = ["masterchefai.pythonanywhere.com"]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
